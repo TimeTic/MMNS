5494dbcfb6027fe5b6e3294b4d4591f90801c7b0
Halimi148 <zahidullah.halimi14@gmail.com>
2023-03-20 06:41:13 -0400

    Added this week's new video

---
+++ about.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>About Us</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="about.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+ 
+
+  <!-- Navigation bar -->
+  <div class="navbar" style="  background-color: #1d2c4d;
+        ">
+        </a>    
+    <a href="index.html">Home</a>
+    <a href="about.html">About us</a>
+    <a href="Event.html">Events</a>
+    <div class="dropdown">
+      <button class="dropbtn">More
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <li><a href="sponsors.html">Sponsors</a></li>
+        <li><a href="showcase.html">Showcase</a></li>
+      </div>
+    </div>
+  </div>
+ 
+
+
+  <br>
+  <br>
+
+  <br>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <main>
+
+
+    <section id="mission">
+      <h2>Our Mission</h2>
+      <p>We are a team of dedicated students who are passionate about delivering exceptional services. Our mission is to
+        provide our community with top News.</p>
+    </section>
+    <section id="team">
+      <h2>Our Team</h2>
+      <div class="profile">
+        <div class="profile-circle">
+          <img src="john-doe.jpg" alt="John Doe">
+        </div>
+        <div class="profile-info">
+          <h3>John Doe</h3>
+          <p>CEO</p>
+        </div>
+      </div>
+      <div class="profile">
+        <div class="profile-circle">
+          <img src="jane-smith.jpg" alt="Jane Smith">
+        </div>
+        <div class="profile-info">
+          <h3>Jane Smith</h3>
+          <p>Marketing Manager</p>
+        </div>
+      </div>
+      <div class="profile">
+        <div class="profile-circle">
+          <img src="bob-johnson.jpg" alt="Bob Johnson">
+        </div>
+        <div class="profile-info">
+          <h3>Bob Johnson</h3>
+          <p>Lead Developer</p>
+        </div>
+      </div>
+    </section>
+    <section id="quality">
+      <h2>Our Commitment to Positivity</h2>
+      <p>We take pride in our work and strive to maintain the highest standards of quality in everything we do.</p>
+    </section>
+    <section id="contact">
+      <h2>Contact info</h2>
+      <p> If you are one of the team member and want to be included here. Email us with your photo, name, grade level,
+        and the class you are in!</p>
+      <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=zahidullah.halimi14@gmail.com"
+        class="contact-button">Contact Us</a>
+
+    </section>
+    <!-- <section id="contact">
+        <h2>Contact Us</h2>
+        <form id="contact-form" method="POST" action="send-email.php">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email">
+          <label for="message">Message:</label>
+          <textarea id="message" name="message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </section> -->
+
+  </main>
+  <!-- Footer -->
+  <footer>
+    <div class="footer-container">
+      <div class="footer-section">
+        <h3>About Us</h3>
+        <p>We are a MBA students, that run this show and our aim is to provide the latest and most relevant information
+          to our viewers about Mertro. Stay updated with us!</p>
+      </div>
+      <div class="footer-section">
+        <h3>Links</h3>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About Us</a></li>
+          <li><a href="Event.html">Events</a></li>
+          <li><a href="sponsors.html">Sponsors</a></li>
+          <li><a href="showcase.html">Showcase</a></li>
+
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h3>Contact Us</h3>
+        <ul>
+          <li>Email: contact@mymedianews.com</li>
+          <li>Phone: 555-555-5555</li>
+          <li>Address: 123 Main St, Anytown USA</li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2023 My Media News Show. All rights reserved.</p>
+    </div>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
